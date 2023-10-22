47e397882fa0130aed3ab1badd826d562415efa9
Karim AMZAL <anoe@macbook-air-de-karim.home>
2023-10-22 17:12:47 +0200

    first commit

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 
+const cors = require("cors");
+
+require("dotenv").config();
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-mongoose.connect("mongodb://localhost/vinted");
+mongoose.connect(process.env.MONGODB_URI);
 
 cloudinary.config({
-  cloud_name: "dlp5aoz6w",
-  api_key: "875812485232484",
-  api_secret: "cV-q2e9k6eU2WFjBAoivO29857Y",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const userRoutes = require("./routes/user");
@@ -23,6 +28,6 @@ app.all("*", (req, res) => {
   res.status(400).json({ message: "This route does not exist" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started");
 });
